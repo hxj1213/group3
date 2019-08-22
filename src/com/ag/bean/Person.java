f7b07305a8d862d613a3dbe612424c57346c6706
yinc <2389617133@qq.com>
2019-08-22 15:33:39 +0800

    create a person

--- src/com/ag/bean/Person.java
+++ src/com/ag/bean/Person.java
@@ -4,4 +4,7 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("hello word");
     }
+    public void Person(){
+        System.out.println("This softwarld is a bug!");
+    }
 }
